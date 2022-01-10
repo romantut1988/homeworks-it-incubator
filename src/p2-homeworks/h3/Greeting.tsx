0ee310770b6dba5d7230a3d0304ece0336c7359e
Roman <romantut1988@mail.ru>
2022-01-10 11:05:36 +0300

    the third task is to determine the class of the input ...

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import s from './Greeting.module.css'
+import React, {ChangeEvent} from 'react'
+import styles from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (setName: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error !== '' ? styles.error : '' // need to fix with (?:)
+    const errorClass = error !== '' ? styles.errorText : ''
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={styles.wrapper}>
+            <div className={styles.columns}>
+                <input value={name} onChange={setNameCallback} className={styles.inputClass + ' ' + inputClass}/>
+                <span className={errorClass}>{error}</span>
+            </div>
+            <div>
+                <button className={styles.button} onClick={addUser}>add</button>
+                <span>{totalUsers}</span>
+            </div>
         </div>
     )
 }
